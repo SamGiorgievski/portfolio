1141e6fff2c8cbf19ed1c921be9545ddcfd71248
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-03-25 15:49:02 -0400

    header setup

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <div className="header">
+        <h1 className="header__title">
+          Sam Giorgievski
+        </h1>
+      </div>
+  )
+}
