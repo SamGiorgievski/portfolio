892c2000867ba3c132e9126aa97e5f0f499b6dfb
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-03-25 16:11:26 -0400

    home page setup

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.scss';
+
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <h1 className="Home__title1">Hi, I'm Sam, a full-stack developer.
+      </h1>
+      <h1 className="Home__title2">Here's a little bit about me, and what i've been working on!</h1>
+      <img className="Home__image" src="https://via.placeholder.com/1000x400"></img>
+    </div>
+  )
+}
