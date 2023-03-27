b61cda1058d926d470f0573dd5098d1eb99e0fd7
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-03-27 12:53:27 -0400

    img description

--- src/Home.js
+++ src/Home.js
@@ -8,7 +8,10 @@ export default function Home() {
       <h1 className="home__title1">Hi, I'm Sam, a full-stack developer.
       </h1>
       <h1 className="home__title2">Here's a little bit about me, and what i've been working on!</h1>
-      <img className="home__image" src="https://via.placeholder.com/1000x400"></img>
+      <div>
+        <img className="home__image" src="https://via.placeholder.com/1000x400"></img>
+        <p> I've been in tech for 8 years. Recently, I was a software project manager / scrum master in charge of delivering features. I completed a web development bootcamp, and have been developing apps since. </p>
+      </div>
     </div>
   )
 }
