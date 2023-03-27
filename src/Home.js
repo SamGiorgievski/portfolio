8c02e800ce43ea23e3ca9ac37c12709a8a38cdb2
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-03-27 13:31:54 -0400

    start projects section

--- src/Home.js
+++ src/Home.js
@@ -5,12 +5,18 @@ import './Home.scss';
 export default function Home() {
   return (
     <div className="home">
-      <h1 className="home__title1">Hi, I'm Sam, a full-stack developer.
-      </h1>
-      <h1 className="home__title2">Here's a little bit about me, and what i've been working on!</h1>
-      <div>
-        <img className="home__image" src="https://via.placeholder.com/1000x400"></img>
-        <p> I've been in tech for 8 years. Recently, I was a software project manager / scrum master in charge of delivering features. I completed a web development bootcamp, and have been developing apps since. </p>
+      <div className="about-me">
+        <h1 className="about-me__title1">Hi, I'm Sam, a full-stack developer.
+        </h1>
+        <h1 className="about-me__title2">Here's a little bit about me, and what i've been working on!</h1>
+        <div className="image-box">
+          <img className="image-box__image" src="https://via.placeholder.com/1000x400"></img>
+          <p className="image-box__text"> I've been in tech for 8 years. Recently, I was a software project manager / scrum master in charge of delivering features. I completed a web development bootcamp, and have been developing apps since. </p>
+        </div>
+      </div>
+
+      <div className="projects">
+        <h1> Projects</h1>
       </div>
     </div>
   )
