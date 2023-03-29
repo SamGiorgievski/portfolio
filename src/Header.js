f2aa6cd4b049b45a0738fe8d27a5612c893d8704
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-03-29 16:12:09 -0400

    header icons

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Header.scss';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Header() {
   return (
@@ -7,6 +9,11 @@ export default function Header() {
         <h1 className="header__title">
           Sam Giorgievski
         </h1>
+        <div className="header__buttons">
+          <Button variant="secondary" className="header__button">Github</Button>
+          <Button variant="secondary" className="header__button">Linkedin</Button>
+          <Button variant="secondary" className="header__button">Secondary</Button>
+        </div>
       </div>
   )
 }
