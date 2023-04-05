2ee886cd1b03d5ab40cf16f243cf3b1a25e6c698
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-04-05 17:20:52 -0400

    navbar routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import Header from "./Header";
 import Home from "./Home";
 import Footer from "./Footer";
+import Projects from "./Projects";
+import { Link, Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
     <div className="App">
+
       <Header />
-      <Home />
+      <Routes>
+        <Route path ="/" index element = {<Home/>}/>
+        <Route path ="/projects" index element = {<Projects/>}/>
+      </Routes>
       <Footer />
 
     </div>
--- src/Header.js
+++ src/Header.js
@@ -2,17 +2,28 @@ import React from 'react';
 import './Header.scss';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
     <div className="header">
-        <h1 className="header__title">
-          Sam Giorgievski
-        </h1>
-        <div className="header__buttons">
-          <Button variant="secondary" className="header__button" href="https://github.com/SamGiorgievski">Github</Button>
-          <Button variant="secondary" className="header__button" href="https://www.linkedin.com/in/samgiorgievski/">Linkedin</Button>
+      <div className="header-flex"> 
+          <h1 className="header__title">
+            Sam Giorgievski
+          </h1>
+          <div className="header__buttons">
+            <Button variant="secondary" className="header__button" href="https://github.com/SamGiorgievski">Github</Button>
+            <Button variant="secondary" className="header__button" href="https://www.linkedin.com/in/samgiorgievski/">Linkedin</Button>
+          </div>
         </div>
+        <ul className="header__nav">
+          <li>
+            <Link to="/" className="header__nav-buttons">About me</Link>
+          </li>
+          <li>
+            <Link to="/projects" className="header__nav-buttons">Projects</Link>
+          </li>
+        </ul>
       </div>
   )
 }
