20099249b435ca7192cc0fb5a53ad55bd158a786
Samuel Giorgievski <SamGiorgievski@gmail.com>
2023-05-08 10:19:50 -0400

    update fonts

--- src/Home.js
+++ src/Home.js
@@ -9,9 +9,9 @@ export default function Home() {
   return (
     <div className="home">
       <div className="about-me">
-        <h1 className="about-me__title1">Hi, I'm Sam, a full-stack developer.
+        <h1 className="about-me__title1">Full-stack developer
         </h1>
-        <h1 className="about-me__title2">Here's a little bit about me, and what I've been working on!</h1>
+        <h1 className="about-me__title2">Here's a little bit about me!</h1>
         <div className="image-box">
           <img className="image-box__image" src="https://via.placeholder.com/1000x400"></img>
           <p className="image-box__text"> I've been in tech for 8 years. Recently, I was a software project manager / scrum master in charge of delivering features. I completed a web development bootcamp, and have been developing apps since. </p>
